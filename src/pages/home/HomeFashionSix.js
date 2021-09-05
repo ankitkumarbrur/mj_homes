6587200dfad7c114aaed5e67a35c466435a1ae25
Ankit Kumar <ak248100@gmail.com>
2021-09-05 17:40:59 +0530

    template alterations

--- src/pages/home/HomeFashionSix.js
+++ src/pages/home/HomeFashionSix.js
@@ -8,33 +8,36 @@ import NewsletterTwo from "../../wrappers/newsletter/NewsletterTwo";
 import ImageSliderOne from "../../wrappers/image-slider/ImageSliderOne";
 
 const HomeFashionSix = () => {
-  return (
-    <Fragment>
-      <MetaTags>
-        <title>Flone | Fashion Home</title>
-        <meta
-          name="description"
-          content="Fashion home of flone react minimalist eCommerce template."
-        />
-      </MetaTags>
-      <LayoutSeven>
-        {/* hero slider */}
-        <HeroSliderFourteen />
-        {/* section title */}
-        <SectionTitleWithText spaceTopClass="pt-95" spaceBottomClass="pb-90" />
-        {/* tab product */}
-        <TabProductEight
-          spaceBottomClass="pb-70"
-          category="fashion"
-          sectionTitle={false}
-        />
-        {/* newsletter */}
-        <NewsletterTwo spaceBottomClass="pb-100" />
-        {/* image slider */}
-        <ImageSliderOne />
-      </LayoutSeven>
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<MetaTags>
+				<title>Flone | Fashion Home</title>
+				<meta
+					name="description"
+					content="Fashion home of flone react minimalist eCommerce template."
+				/>
+			</MetaTags>
+			<LayoutSeven>
+				{/* hero slider */}
+				<HeroSliderFourteen />
+				{/* section title */}
+				<SectionTitleWithText
+					spaceTopClass="pt-95"
+					spaceBottomClass="pb-90"
+				/>
+				{/* tab product */}
+				<TabProductEight
+					spaceBottomClass="pb-70"
+					category="fashion"
+					sectionTitle={false}
+				/>
+				{/* newsletter */}
+				<NewsletterTwo spaceBottomClass="pb-100" />
+				{/* image slider */}
+				<ImageSliderOne />
+			</LayoutSeven>
+		</Fragment>
+	);
 };
 
 export default HomeFashionSix;
--- src/pages/home/HomeFurnitureFour.js
+++ src/pages/home/HomeFurnitureFour.js
@@ -11,50 +11,54 @@ import CategoryFiveGrid from "../../wrappers/category/CategoryFiveGrid";
 import HeroSliderTwentyNine from "../../wrappers/hero-slider/HeroSliderTwentyNine";
 
 const HomeFurnitureFour = () => {
-  return (
-    <Fragment>
-      <MetaTags>
-        <title>Flone | Furniture Home</title>
-        <meta
-          name="description"
-          content="Furniture home of flone react minimalist eCommerce template."
-        />
-      </MetaTags>
-      <LayoutOne
-        headerContainerClass="container-fluid"
-        headerPaddingClass="header-padding-1"
-        headerPositionClass="header-absolute"
-      >
-        {/* hero slider */}
-        <HeroSliderTwentyNine />
-        {/* category grid */}
-        <CategoryFiveGrid spaceBottomClass="pb-70" />
+	return (
+		<Fragment>
+			<MetaTags>
+				<title>Flone | Furniture Home</title>
+				<meta
+					name="description"
+					content="Furniture home of flone react minimalist eCommerce template."
+				/>
+			</MetaTags>
+			<LayoutOne
+				headerContainerClass="container-fluid"
+				headerPaddingClass="header-padding-1"
+				headerPositionClass="header-absolute"
+			>
+				{/* hero slider */}
+				<HeroSliderTwentyNine />
+				{/* category grid */}
+				<CategoryFiveGrid spaceBottomClass="pb-70" />
 
-        {/* feature icon */}
-        <FeatureIconSeven spaceBottomClass="pb-70" />
-        {/* video popup */}
-        <VideoPopupTwo />
-        {/* tab product */}
-        <TabProductNineteen
-          spaceTopClass="pt-95"
-          spaceBottomClass="pb-100"
-          category="furniture"
-        />
-        {/* countdown */}
-        <CountDownFive
-          spaceTopClass="pt-115"
-          spaceBottomClass="pb-115"
-          bgImg="/assets/img/bg/bg-6.jpg"
-          image="/assets/img/banner/deal-9.png"
-          dateTime="November 13, 2021 12:12:00"
-        />
-        {/* brand logo slider */}
-        <BrandLogoSliderOne spaceBottomClass="pb-95" spaceTopClass="pt-100" />
-        {/* image slider */}
-        <ImageSliderTwo />
-      </LayoutOne>
-    </Fragment>
-  );
+				{/* feature icon */}
+				<FeatureIconSeven spaceBottomClass="pb-70" />
+
+				{/* video popup */}
+				<VideoPopupTwo />
+				{/* tab product */}
+				<TabProductNineteen
+					spaceTopClass="pt-95"
+					spaceBottomClass="pb-100"
+					category="furniture"
+				/>
+				{/* countdown */}
+				<CountDownFive
+					spaceTopClass="pt-115"
+					spaceBottomClass="pb-115"
+					bgImg="/assets/img/bg/bg-6.jpg"
+					image="/assets/img/banner/deal-9.png"
+					dateTime="November 13, 2021 12:12:00"
+				/>
+				{/* brand logo slider */}
+				<BrandLogoSliderOne
+					spaceBottomClass="pb-95"
+					spaceTopClass="pt-100"
+				/>
+				{/* image slider */}
+				<ImageSliderTwo />
+			</LayoutOne>
+		</Fragment>
+	);
 };
 
 export default HomeFurnitureFour;
