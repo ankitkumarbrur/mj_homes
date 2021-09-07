10685e8e2c308d94dd02c4605a1d5294a6ab0014
Ankit Kumar <ak248100@gmail.com>
2021-09-07 22:56:46 +0530

    images in two columns

--- src/pages/shop-product/ProductSticky.js
+++ src/pages/shop-product/ProductSticky.js
@@ -10,62 +10,64 @@ import ProductDescriptionTab from "../../wrappers/product/ProductDescriptionTab"
 import ProductImageDescriptionSticky from "../../wrappers/product/ProductImageDescriptionSticky";
 
 const ProductSticky = ({ location, product }) => {
-  const { pathname } = location;
+	const { pathname } = location;
 
-  return (
-    <Fragment>
-      <MetaTags>
-        <title>Flone | Product Page</title>
-        <meta
-          name="description"
-          content="Product page of flone react minimalist eCommerce template."
-        />
-      </MetaTags>
+	return (
+		<Fragment>
+			<MetaTags>
+				<title>Flone | Product Page</title>
+				<meta
+					name="description"
+					content="Product page of flone react minimalist eCommerce template."
+				/>
+			</MetaTags>
 
-      <BreadcrumbsItem to={process.env.PUBLIC_URL + "/"}>Home</BreadcrumbsItem>
-      <BreadcrumbsItem to={process.env.PUBLIC_URL + pathname}>
-        Shop Product
-      </BreadcrumbsItem>
+			<BreadcrumbsItem to={process.env.PUBLIC_URL + "/"}>
+				Home
+			</BreadcrumbsItem>
+			<BreadcrumbsItem to={process.env.PUBLIC_URL + pathname}>
+				Shop Product
+			</BreadcrumbsItem>
 
-      <LayoutOne headerTop="visible">
-        {/* breadcrumb */}
-        <Breadcrumb />
+			<LayoutOne headerTop="visible">
+				{/* breadcrumb */}
+				<Breadcrumb />
 
-        {/* product description with image */}
-        <ProductImageDescriptionSticky
-          spaceTopClass="mt-100"
-          spaceBottomClass="mb-100"
-          product={product}
-        />
+				{/* product description with image */}
+				<ProductImageDescriptionSticky
+					spaceTopClass="mt-100"
+					spaceBottomClass="mb-100"
+					product={product}
+				/>
 
-        {/* product description tab */}
-        <ProductDescriptionTab
-          spaceBottomClass="pb-90"
-          productFullDesc={product.fullDescription}
-        />
+				{/* product description tab */}
+				<ProductDescriptionTab
+					spaceBottomClass="pb-90"
+					productFullDesc={product.fullDescription}
+				/>
 
-        {/* related product slider */}
-        <RelatedProductSlider
-          spaceBottomClass="pb-95"
-          category={product.category[0]}
-        />
-      </LayoutOne>
-    </Fragment>
-  );
+				{/* related product slider */}
+				<RelatedProductSlider
+					spaceBottomClass="pb-95"
+					category={product.category[0]}
+				/>
+			</LayoutOne>
+		</Fragment>
+	);
 };
 
 ProductSticky.propTypes = {
-  location: PropTypes.object,
-  product: PropTypes.object,
+	location: PropTypes.object,
+	product: PropTypes.object,
 };
 
 const mapStateToProps = (state, ownProps) => {
-  const itemId = ownProps.match.params.id;
-  return {
-    product: state.productData.products.filter(
-      (single) => single.id === itemId
-    )[0],
-  };
+	const itemId = ownProps.match.params.id;
+	return {
+		product: state.productData.products.filter(
+			(single) => single.id === itemId,
+		)[0],
+	};
 };
 
 export default connect(mapStateToProps)(ProductSticky);
