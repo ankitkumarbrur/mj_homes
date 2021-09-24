fefc1d940843b19137a4a59d79ccd77ceac8c0eb
salonisv22 <77237060+salonisv22@users.noreply.github.com>
2021-09-24 18:39:55 +0530

    order

--- api/carts/urls.py
+++ api/carts/urls.py
@@ -1,17 +1,12 @@
 from .models import WishList
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+
 
 from .views import CartView, WishListView
 
-router = DefaultRouter()
 
-router.register('cart', CartView, basename = "cart")
 
 
 urlpatterns = [
-    # path('cart/', CartView.as_view(), name = 'cartList'),
-    # path('cart/<int:pk>', CartView.as_view(), name = 'cartList'),
-    path('', include(router.urls)),
-    path('wishlist/',WishListView.as_view(),name = 'wishlist')
+    
 ]
--- api/orders/views.py
+++ api/orders/views.py
@@ -1,18 +1,9 @@
-from django.shortcuts import render
-from rest_framework.response import Response
-from .models import Cart,CartItem
-from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import ListModelMixin
-from rest_framework import permissions
+from .models import Order
+from users.models import User
+from rest_framework import viewsets
 from .serializers import CartSerializer
 
-# Create your views here.
-class Cart_view(GenericAPIView, ListModelMixin, ):
-    queryset = Cart.objects.all()
-    serializer_class = CartSerializer
-    # permission_classes = [permissions.IsAuthenticated]
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-    
-   
+class OrderView(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = CartSerializer   
