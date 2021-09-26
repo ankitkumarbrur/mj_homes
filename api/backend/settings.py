9e3a9836df63b6236d62f17e39cc5bacc83f543c
Ankit Kumar <ak248100@gmail.com>
2021-09-26 17:35:09 +0530

    added cors all

--- api/backend/settings.py
+++ api/backend/settings.py
@@ -160,9 +160,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS & CSRF configuration
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000'
-]
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3000'
+# ]
 
 # CSRF_TRUSTED_ORIGINS = [
 #     'http://siteyouwantto.allow.com',
