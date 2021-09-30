ca656991997a4607de510d831bfac4c44dbe939e
Ankit Kumar <ak248100@gmail.com>
2021-09-30 14:22:31 +0530

    added items in whitelist

--- api/backend/settings.py
+++ api/backend/settings.py
@@ -164,10 +164,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
-# CORS_ALLOWED_ORIGINS = [
-#     'http://localhost:3000',
-#     'http://localhost:3001',
-# ]
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'https://ankitbrur.pythonanywhere.com'
+]
 
 # CSRF_TRUSTED_ORIGINS = [
 #     'http://localhost:3000',
